99265255115e85f9b82e167a3e2fcf1927ab5007
huayemao <huayemao4g@gmail.com>
2023-07-16 15:54:25 +0800

    feat: 文件上传后缀名限制

--- components/GenerateForm.vue
+++ components/GenerateForm.vue
@@ -39,68 +39,37 @@ const gradeName = GRADE_MAPPING[gradeKey];
   <div class="">
     <div class="mb-4 flex flex-col justify-between md:flex-row md:items-center">
       <div
-        class="ltablet:max-w-full flex max-w-[425px] flex-col items-center gap-4 text-center md:flex-row md:text-left lg:max-w-full"
-      >
+        class="ltablet:max-w-full flex max-w-[425px] flex-col items-center gap-4 text-center md:flex-row md:text-left lg:max-w-full">
         <div>
-          <h2
-            class="font-heading text-xl font-light leading-tight text-muted-800 dark:text-white"
-          >
+          <h2 class="font-heading text-xl font-light leading-tight text-muted-800 dark:text-white">
             <span>生成报表</span>
           </h2>
           <p class="font-alt text-sm font-normal leading-normal">
             <span class="text-muted-500">
-              导入年级数据：包括学生成绩、教师、学校等</span
-            >
+              导入年级数据：包括学生成绩、教师、学校等</span>
           </p>
         </div>
       </div>
-      <div
-        class="mt-4 flex items-center justify-center gap-2 md:mt-0 md:justify-start"
-      >
-        <button
-          type="button"
-          class="relative font-sans font-normal text-sm inline-flex items-center justify-center leading-5 no-underline h-8 px-3 py-2 space-x-1 border nui-focus transition-all duration-300 disabled:opacity-60 disabled:cursor-not-allowed hover:enabled:shadow-none text-muted-700 bg-white border-muted-300 dark:text-white dark:bg-muted-700 dark:border-muted-600 dark:hover:enabled:bg-muted-600 hover:enabled:bg-muted-50 dark:active:enabled:bg-muted-700/70 active:enabled:bg-muted-100 rounded-md"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            aria-hidden="true"
-            role="img"
-            class="icon h-3 w-3"
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m12 19l-7-7l7-7m7 7H5"
-            ></path></svg
-          ><span>取消</span>
+      <div class="mt-4 flex items-center justify-center gap-2 md:mt-0 md:justify-start">
+        <button type="button"
+          class="relative font-sans font-normal text-sm inline-flex items-center justify-center leading-5 no-underline h-8 px-3 py-2 space-x-1 border nui-focus transition-all duration-300 disabled:opacity-60 disabled:cursor-not-allowed hover:enabled:shadow-none text-muted-700 bg-white border-muted-300 dark:text-white dark:bg-muted-700 dark:border-muted-600 dark:hover:enabled:bg-muted-600 hover:enabled:bg-muted-50 dark:active:enabled:bg-muted-700/70 active:enabled:bg-muted-100 rounded-md">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img"
+            class="icon h-3 w-3" width="1em" height="1em" viewBox="0 0 24 24">
+            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="m12 19l-7-7l7-7m7 7H5"></path>
+          </svg><span>取消</span>
         </button>
       </div>
     </div>
     <div
-      class="border-muted-200 dark:border-muted-700 dark:bg-muted-800 relative w-full border bg-white transition-all duration-300 rounded-md"
-    >
-      <form
-        @submit.prevent="handleSubmit"
-        action=""
-        class="divide-muted-200 dark:divide-muted-700 grid divide-x sm:grid-cols-2"
-      >
-        <div
-          shape="curved"
-          class="bg-muted-50 dark:bg-muted-800/60 space-y-8 p-10"
-        >
+      class="border-muted-200 dark:border-muted-700 dark:bg-muted-800 relative w-full border bg-white transition-all duration-300 rounded-md">
+      <form @submit.prevent="handleSubmit" action=""
+        class="divide-muted-200 dark:divide-muted-700 grid divide-x sm:grid-cols-2">
+        <div shape="curved" class="bg-muted-50 dark:bg-muted-800/60 space-y-8 p-10">
           <div class="mx-auto flex w-full max-w-[410px] flex-col">
             <div>
               <div>
-                <div
-                  class="relative mb-5 flex flex-col items-center justify-center gap-4"
-                >
+                <div class="relative mb-5 flex flex-col items-center justify-center gap-4">
                   <div class="relative">
                     2022元谋县教学质量分析报表——{{ gradeName }}
                   </div>
@@ -108,7 +77,12 @@ const gradeName = GRADE_MAPPING[gradeKey];
                 <div class="grid grid-cols-12 gap-4">
                   <div class="col-span-12">
                     <div class="relative">
-                      <FileInput v-model="xslx" id="xlsx" />
+                      <BaseInputFile v-model="xslx" :text-value='(fileList: FileList | null) => {
+                        if (!fileList?.item?.length) {
+                          return "未选择文件";
+                        } return fileList?.item.length === 1 ? fileList.item(0)?.name
+                          ?? "Invalid file selected" : `${fileList?.item?.length ?? 0} files selected`;
+                      }' id="xlsx" :label="`上传${gradeName}成绩数据`" placeholder="选择文件" accept=".xlsx, .xls" />
                     </div>
                   </div>
                 </div>
@@ -122,85 +96,35 @@ const gradeName = GRADE_MAPPING[gradeKey];
             <div class="grid grid-cols-12 gap-4">
               <div class="col-span-12">
                 <div class="relative gap-1 text-muted-400">
-                  <div
-                    class="text-muted-500 dark:text-muted-400 mb-2 select-none font-sans text-sm"
-                  >
+                  <div class="text-muted-500 dark:text-muted-400 mb-2 select-none font-sans text-sm">
                     权重配置
                   </div>
-                  <div
-                    class="absolute top-0 right-0 inline-flex items-center gap-1 text-muted-400"
-                  >
-                    <CheckBox
-                      label="按区域配置权重"
-                      v-model="byRegion"
-                      id="byRegion"
-                    ></CheckBox>
+                  <div class="absolute top-0 right-0 inline-flex items-center gap-1 text-muted-400">
+                    <CheckBox label="按区域配置权重" v-model="byRegion" id="byRegion"></CheckBox>
                   </div>
-                  <el-table
-                    v-if="!!byRegion"
-                    :data="DEFAULT_TEACHER_METRIC_CONFIG_BY_REGION"
-                    striped
-                    class="border"
-                  >
-                    <el-table-column
-                      header-align="center"
-                      align="center"
-                      prop="name"
-                      label="类别"
-                      label-class-name="text-center"
-                    />
-                    <el-table-column
-                      header-align="center"
-                      align="center"
-                      prop="averageScore"
-                      label="平均分"
-                      label-class-name="text-center"
-                    />
-                    <el-table-column
-                      header-align="center"
-                      align="center"
-                      prop="qualifiedRate"
-                      label="合格率"
-                      label-class-name="text-center"
-                    />
-                    <el-table-column
-                      header-align="center"
-                      align="center"
-                      prop="excellentRate"
-                      label="优生率"
-                      label-class-name="text-center"
-                    />
+                  <el-table v-if="!!byRegion" :data="DEFAULT_TEACHER_METRIC_CONFIG_BY_REGION" striped class="border">
+                    <el-table-column header-align="center" align="center" prop="name" label="类别"
+                      label-class-name="text-center" />
+                    <el-table-column header-align="center" align="center" prop="averageScore" label="平均分"
+                      label-class-name="text-center" />
+                    <el-table-column header-align="center" align="center" prop="qualifiedRate" label="合格率"
+                      label-class-name="text-center" />
+                    <el-table-column header-align="center" align="center" prop="excellentRate" label="优生率"
+                      label-class-name="text-center" />
                   </el-table>
                 </div>
               </div>
               <div class="col-span-12" v-if="!!byRegion">
                 <div class="relative w-full">
-                  <div
-                    class="text-muted-500 dark:text-muted-400 mb-2 select-none font-sans text-sm"
-                  >
+                  <div class="text-muted-500 dark:text-muted-400 mb-2 select-none font-sans text-sm">
                     学校-区域划分
                   </div>
 
-                  <el-table
-                    :data="SCHOOLS"
-                    height="200px"
-                    striped
-                    class="border w-full"
-                  >
-                    <el-table-column
-                      header-align="center"
-                      align="center"
-                      prop="区域类别"
-                      label="区域"
-                      label-class-name="text-center"
-                    />
-                    <el-table-column
-                      header-align="center"
-                      align="center"
-                      prop="学校名称"
-                      label="学校"
-                      label-class-name="text-center"
-                    />
+                  <el-table :data="SCHOOLS" height="200px" striped class="border w-full">
+                    <el-table-column header-align="center" align="center" prop="区域类别" label="区域"
+                      label-class-name="text-center" />
+                    <el-table-column header-align="center" align="center" prop="学校名称" label="学校"
+                      label-class-name="text-center" />
                   </el-table>
                 </div>
               </div>
@@ -241,12 +165,8 @@ const gradeName = GRADE_MAPPING[gradeKey];
                 </div>
               </div> -->
             </div>
-            <div
-              class="mt-5 flex flex-col-reverse text-right md:block md:space-x-3"
-            >
-              <Button :disabled="loading" :loading="loading" type="submit"
-                >生成报表</Button
-              >
+            <div class="mt-5 flex flex-col-reverse text-right md:block md:space-x-3">
+              <Button :disabled="loading" :loading="loading" type="submit">生成报表</Button>
               <!-- <button
                 type="button"
                 class="is-button rounded is-button-default w-full sm:w-32"
