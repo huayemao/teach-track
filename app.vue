b6c227552a8425ddab8a9579c36b403143ad5667
huayemao <huayemao4g@gmail.com>
2023-07-19 15:59:24 +0800

    feat: 教师结果支持筛选学校和年级

--- app.vue
+++ app.vue
@@ -763,60 +763,6 @@ onMounted(() => {
           <div class="ms-auto"></div>
           <div class="flex items-center gap-2 h-16">
             <BaseThemeToggle />
-
-            <div class="group inline-flex items-center justify-center text-right">
-              <div data-headlessui-state="" class="relative h-9 w-9 text-left">
-                <div id="headlessui-menu-button-315" aria-haspopup="menu" aria-expanded="false" data-headlessui-state="">
-                  <button type="button"
-                    class="group-hover:ring-muted-200 dark:group-hover:ring-muted-700 dark:ring-offset-muted-900 inline-flex h-9 w-9 items-center justify-center rounded-full ring-1 ring-transparent transition-all duration-300 group-hover:ring-offset-4">
-                    <span
-                      class="border-muted-200 dark:border-muted-700 dark:bg-muted-800 flex h-9 w-9 items-center justify-center rounded-full border bg-white"><svg
-                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
-                        role="img" class="icon text-muted-400 h-5 w-5" style="" width="1em" height="1em"
-                        viewBox="0 0 256 256" data-v-cd102a71="">
-                        <g fill="currentColor">
-                          <path
-                            d="M208 192H48a8 8 0 0 1-6.88-12C47.71 168.6 56 139.81 56 104a72 72 0 0 1 144 0c0 35.82 8.3 64.6 14.9 76a8 8 0 0 1-6.9 12Z"
-                            opacity=".2"></path>
-                          <path
-                            d="M221.8 175.94c-5.55-9.56-13.8-36.61-13.8-71.94a80 80 0 1 0-160 0c0 35.34-8.26 62.38-13.81 71.94A16 16 0 0 0 48 200h40.81a40 40 0 0 0 78.38 0H208a16 16 0 0 0 13.8-24.06ZM128 216a24 24 0 0 1-22.62-16h45.24A24 24 0 0 1 128 216Zm-80-32c7.7-13.24 16-43.92 16-80a64 64 0 1 1 128 0c0 36.05 8.28 66.73 16 80Z">
-                          </path>
-                        </g>
-                      </svg></span>
-                  </button>
-                </div>
-                <!---->
-              </div>
-            </div>
-            <!--]--><!--[--><button type="button"
-              class="border-muted-200 hover:ring-muted-200 dark:hover:ring-muted-700 dark:border-muted-700 dark:bg-muted-800 dark:ring-offset-muted-900 flex h-9 w-9 items-center justify-center rounded-full border bg-white ring-1 ring-transparent transition-all duration-300 hover:ring-offset-4">
-              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
-                role="img" class="icon text-muted-400 h-5 w-5" style="" width="1em" height="1em" viewBox="0 0 256 256"
-                data-v-cd102a71="">
-                <g fill="currentColor">
-                  <path
-                    d="M112 80a32 32 0 1 1-32-32a32 32 0 0 1 32 32Zm64 32a32 32 0 1 0-32-32a32 32 0 0 0 32 32Zm-96 32a32 32 0 1 0 32 32a32 32 0 0 0-32-32Zm96 0a32 32 0 1 0 32 32a32 32 0 0 0-32-32Z"
-                    opacity=".2"></path>
-                  <path
-                    d="M80 40a40 40 0 1 0 40 40a40 40 0 0 0-40-40Zm0 64a24 24 0 1 1 24-24a24 24 0 0 1-24 24Zm96 16a40 40 0 1 0-40-40a40 40 0 0 0 40 40Zm0-64a24 24 0 1 1-24 24a24 24 0 0 1 24-24Zm-96 80a40 40 0 1 0 40 40a40 40 0 0 0-40-40Zm0 64a24 24 0 1 1 24-24a24 24 0 0 1-24 24Zm96-64a40 40 0 1 0 40 40a40 40 0 0 0-40-40Zm0 64a24 24 0 1 1 24-24a24 24 0 0 1-24 24Z">
-                  </path>
-                </g>
-              </svg></button><!--]--><!--[-->
-            <div class="group inline-flex items-center justify-center text-right">
-              <div data-headlessui-state="" class="relative h-9 w-9 text-left">
-                <button
-                  class="group-hover:ring-primary-500 dark:ring-offset-muted-900 inline-flex h-9 w-9 items-center justify-center rounded-full ring-1 ring-transparent transition-all duration-300 group-hover:ring-offset-4"
-                  id="headlessui-menu-button-317" aria-haspopup="menu" aria-expanded="false" type="button">
-                  <div class="relative inline-flex h-9 w-9 items-center justify-center rounded-full">
-                    <!-- <img
-                    src="/img/avatars/2.svg"
-                    class="max-w-full rounded-full object-cover shadow-sm dark:border-transparent"
-                    alt=""
-                  /> -->
-                  </div>
-                </button>
-              </div>
-            </div>
           </div>
         </div>
         <main style="min-height: calc( 100vh - 6rem )">
--- components/TeacherResultTable.vue
+++ components/TeacherResultTable.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { TeacherInfo } from "@/utils/process";
+import { TableColumnCtx } from "element-plus/es/components";
 
 // todo: header 要 sticky
 // todo: 多级表头
@@ -7,13 +8,27 @@ import { TeacherInfo } from "@/utils/process";
 const { teachers } = defineProps<{
   teachers: TeacherInfo[];
 }>();
+
+const dedupe = (arr: any[]) => Array.from(new Set(arr))
+
+const filterableCols = ['学校', '年级']
+const filterHandler = (
+  value: TeacherInfo,
+  row: object,
+  column: TableColumnCtx<TeacherInfo>
+) => {
+  const property = column['property']
+  return row[property] === value
+}
+
 </script>
 
 <template>
   <el-table :data="teachers" striped v-if="teachers?.length" class="h-full" style="overflow:unset">
     <template v-for="key in Object.keys(teachers[0])">
-      <el-table-column v-if="['number', 'string'].includes(typeof teachers[0][key])" sortable header-align="center"
-        align="center" :prop="key" :label="key" label-class-name="text-center" :formatter="typeof teachers[0][key] === 'number' && !key.includes('数')
+      <el-table-column v-if="['number', 'string'].includes(typeof teachers[0][key])" sortable
+        :filters="filterableCols.includes(key) ? dedupe(teachers.map(e => e[key])).map(e => ({ text: e, value: e })) : undefined"
+        header-align="center" align="center" :prop="key" :label="key" label-class-name="text-center" :formatter="typeof teachers[0][key] === 'number' && !key.includes('数')
           ? (row) =>
             Number(row[key])
               .toLocaleString('zh-CN', {
@@ -22,7 +37,7 @@ const { teachers } = defineProps<{
               })
               .replace(/,/g, '')
           : undefined
-          " />
+          " :filter-method="filterableCols.includes(key) ? filterHandler : undefined" />
     </template>
   </el-table>
 </template>
